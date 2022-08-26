5a12acebf38f17287cb2deb2fd321e302482e674
AyaNADINI <90606457+AyaNADINI@users.noreply.github.com>
2022-08-26 11:44:24 +0200

    feat(webconference): #18 add git action workflow and unit test (#20)

---
+++ src/main/resources/public/ts/services/_tests_/SimpleCI.test.ts
@@ -0,0 +1,13 @@
+describe("Simple test", () => {
+    describe("method test", () => {
+        describe("when a < 0", () => {
+            it("throwns an exception", () => { });
+        });
+        describe("when a == 0", () => {
+            it("returns 0", () => { });
+        });
+        describe("when a > 0", () => {
+            it("returns xyz", () => { });
+        });
+    });
+});
